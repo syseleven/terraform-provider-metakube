5202e6f67a2ce30924aad0e26e2c33587df11668
Furkhat Kasymovgeniiuulu <vailodf@gmail.com>
2022-01-18 10:53:50 +0100

    use production to prepare values for tests

--- scripts/get_versions.sh
+++ scripts/get_versions.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl 'https://stage.metakube.de/api/v1/upgrades/cluster?type=kubernetes' -H "Authorization: Bearer ${METAKUBE_TOKEN}"
+curl 'https://metakube.syseleven.de/api/v1/upgrades/cluster?type=kubernetes' -H "Authorization: Bearer ${METAKUBE_TOKEN}"
