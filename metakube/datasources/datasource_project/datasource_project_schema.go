68a78dfbedb31738e013419a9f7844e16318c8a4
calebikhuohon <calebikhuohon@gmail.com>
2025-12-02 16:35:48 +0100

    feat: migrate project datasource

---
+++ metakube/datasources/datasource_project/datasource_project_schema.go
@@ -0,0 +1,29 @@
+package datasource_project
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func DataSourceProjectSchema() schema.Schema {
+	return schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+			},
+			"name": schema.StringAttribute{
+				Required: true,
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+				},
+			},
+		},
+	}
+}
+
+type metakubeProjectDataSourceModel struct {
+	ID   types.String `tfsdk:"id"`
+	Name types.String `tfsdk:"name"`
+}
