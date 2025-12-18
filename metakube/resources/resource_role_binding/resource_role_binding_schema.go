8d8618c1a2498927522ee02d85f0fe790af7fa63
Mert Dogan <m.dogan@syseleven.de>
2025-12-18 15:19:20 +0100

    migrate role binding resource

---
+++ metakube/resources/resource_role_binding/resource_role_binding_schema.go
@@ -0,0 +1,106 @@
+package resource_role_binding
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func RoleBindingSchema() schema.Schema {
+	return schema.Schema{
+		Attributes: metakubeRoleBindingAttributes(),
+		Blocks:     metakubeRoleBindingSubjectBlock(),
+	}
+}
+
+// RoleBindingModel represents the Terraform resource model for a role binding.
+type RoleBindingModel struct {
+	ID        types.String `tfsdk:"id"`
+	ProjectID types.String `tfsdk:"project_id"`
+	ClusterID types.String `tfsdk:"cluster_id"`
+	Namespace types.String `tfsdk:"namespace"`
+	RoleName  types.String `tfsdk:"role_name"`
+	Subject   types.List   `tfsdk:"subject"`
+}
+
+// SubjectModel represents the subject block.
+type SubjectModel struct {
+	Kind types.String `tfsdk:"kind"`
+	Name types.String `tfsdk:"name"`
+}
+
+func metakubeSubjectAttrTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"kind": types.StringType,
+		"name": types.StringType,
+	}
+}
+
+func metakubeRoleBindingAttributes() map[string]schema.Attribute {
+	return map[string]schema.Attribute{
+		"id": schema.StringAttribute{
+			Computed:    true,
+			Description: "The id of the role binding resource",
+		},
+		"project_id": schema.StringAttribute{
+			Required: true,
+			Validators: []validator.String{
+				stringvalidator.LengthAtLeast(1),
+			},
+			Description: "The id of the project resource belongs to",
+		},
+		"cluster_id": schema.StringAttribute{
+			Required: true,
+			Validators: []validator.String{
+				stringvalidator.LengthAtLeast(1),
+			},
+			Description: "The id of the cluster resource belongs to",
+		},
+		"namespace": schema.StringAttribute{
+			Required: true,
+			Validators: []validator.String{
+				stringvalidator.LengthAtLeast(1),
+			},
+			Description: "The name of the namespace",
+		},
+		"role_name": schema.StringAttribute{
+			Required: true,
+			Validators: []validator.String{
+				stringvalidator.LengthAtLeast(1),
+			},
+			Description: "The name of the role to bind to",
+		},
+	}
+}
+
+func metakubeRoleBindingSubjectBlock() map[string]schema.Block {
+	return map[string]schema.Block{
+		"subject": schema.ListNestedBlock{
+			Validators: []validator.List{
+				listvalidator.SizeAtLeast(1),
+			},
+			Description: "Users and groups to bind for",
+			NestedObject: schema.NestedBlockObject{
+				Attributes: map[string]schema.Attribute{
+					"kind": schema.StringAttribute{
+						Required:    true,
+						Description: "Can be either 'user' or 'group'",
+						Validators: []validator.String{
+							stringvalidator.OneOf("user", "group"),
+						},
+					},
+					"name": schema.StringAttribute{
+						Optional:    true,
+						Description: "Subject name",
+						Validators: []validator.String{
+							stringvalidator.LengthAtLeast(1),
+						},
+					},
+				},
+			},
+		},
+	}
+}
